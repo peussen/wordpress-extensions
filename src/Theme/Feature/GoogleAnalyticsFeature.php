1f90cfdf5dbd8545e2c1bada819674b7ecf0eb20
Peter Eussen <peter.eussen@harperjones.nl>
2015-12-07 16:38:07 +0100

    Theme support improvements

--- src/Theme/Feature/GoogleAnalyticsFeature.php
+++ src/Theme/Feature/GoogleAnalyticsFeature.php
@@ -9,6 +9,17 @@ namespace HarperJones\Wordpress\Theme\Feature;
 
 use HarperJones\Wordpress\GoogleAnalytics;
 
+/**
+ * Usage:
+ *
+ * in your code you should have two things:
+ *
+ * <code>
+ * add_theme_support('harperjones-googleanalytics','UA-XXXXXX-XX');
+ * </code>
+ *
+ * @package HarperJones\Wordpress\Theme\Feature
+ */
 class GoogleAnalyticsFeature implements FeatureInterface
 {
     private $google;
@@ -18,7 +29,7 @@ class GoogleAnalyticsFeature implements FeatureInterface
         if (isset($options['ua'])) {
             $ua = $options['ua'];
         } else {
-            $ua = null;
+            $ua = (isset($options[0]) ? $options[0] : false);
         }
 
         $this->google = new GoogleAnalytics($ua,$options);
