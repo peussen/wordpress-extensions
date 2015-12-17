b5d9e75869d9ec9908e6c9971a9b0d6e2b189b1b
Peter Eussen <peter.eussen@harperjones.nl>
2015-12-17 11:22:53 +0100

    theme support for varnish

---
+++ src/Theme/Feature/Varnish.php
@@ -0,0 +1,138 @@
+<?php
+/*
+ * @author: petereussen
+ * @package: wordpress-extensions
+ */
+
+namespace HarperJones\Wordpress\Theme\Feature;
+
+/**
+ * Adds support for varnish caching
+ * When enabled, this feature will  hook into various actions to detect
+ * changes to posts, and initiate a flush when it happens, so the cache is never
+ * out of date.
+ *
+ * To enable this, add the following line to your theme init:
+ *
+ * <code>
+ * add_theme_support('harperjones-varnish');
+ * </code>
+ *
+ * Additionally you can add extra configuration options:
+ * - server: the server to use to initiate purge commands
+ * - port: the port to use to initiate a purge request
+ * - host: the domain to use as purge filter
+ *
+ * @package HarperJones\Wordpress\Theme\Feature
+ */
+class Varnish implements FeatureInterface
+{
+    protected $varnishServerIP = false;
+    protected $varnishPort     = false;
+    protected $flushHost       = false;
+    protected $actions         = array(
+        'save_post',
+        'deleted_post',
+        'trashed_post',
+        'edit_post',
+        'delete_attachment',
+        'switch_theme',
+        'publish_post',
+        'transition_post_status',
+    );
+
+    public function register($options = [])
+    {
+        if (isset($options['server'])) {
+            $this->setVarnishIp($options['server']);
+        }
+
+        if ( isset($options['port'])) {
+            $this->setVarnishPort($options['port']);
+        }
+
+        if ( isset($options['host'])) {
+            $this->setHost($options['host']);
+        }
+
+        if ($this->varnishServerIP || $this->detectVarnishSetup()) {
+            $this->addHooks();
+        }
+    }
+
+    /**
+     * Changes the varnish IP used for purges to the one given
+     *
+     * @param string $ip
+     */
+    public function setVarnishIp( $ip )
+    {
+        $this->varnishServerIP = apply_filters('hj_varnish_server_ip',$ip);
+    }
+
+    /**
+     * Changes the port the varnish server listens on to the one specified
+     *
+     * @param int $port
+     */
+    public function setVarnishPort( $port )
+    {
+        $this->varnishPost = apply_filters('hj_varnish_server_port',$port);
+    }
+
+    /**
+     * Change the default domain to use for flushing site content
+     *
+     * @param string $host
+     */
+    public function setHost($host)
+    {
+        $this->flushHost = $host;
+    }
+
+    /**
+     * execute a flush whenever a post has changed
+     *
+     */
+    public function executeFlush()
+    {
+        $url          = 'http://' . $this->varnishServerIP . ($this->varnishPort ? ':' . $this->varnishPort : '');
+        $purgeRequest = [
+            'method'    => 'PURGE',
+            'headers'   => [
+                'host'              => apply_filters('hj_varnish_purge_domain',$this->flushHost),
+                'X-Purge-Method'    => 'regex'
+            ]
+        ];
+
+        wp_remote_request($url,$purgeRequest);
+    }
+
+    /**
+     * Hook on all specified actions
+     */
+    private function addHooks()
+    {
+        foreach ($this->actions as $action) {
+            add_action($action,[$this,'executeFlush']);
+        }
+    }
+
+    /**
+     * Try to detect if we have varnish automatically based on the request
+     *
+     * @return bool
+     */
+    private function detectVarnishSetup()
+    {
+        if ( isset($_SERVER['HTTP_X_VARNISH'])) {
+            if ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) && isset($_SERVER['REMOTE_ADDR'])) {
+                $this->setVarnishIP($_SERVER['REMOTE_ADDR']);
+                $this->setHost(parse_url(get_bloginfo('url'),PHP_URL_HOST));
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
