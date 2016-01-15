0602f9779a7d23f9eedbef65b1715aefe208f1d1
Peter Eussen <peter.eussen@harperjones.nl>
2016-01-15 15:54:03 +0100

    Simple setup to avoid someone from asking posttypes we do not want to expose

---
+++ src/Theme/Feature/HidePostTypeFeature.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * @author: petereussen
+ * @package: wordpress-extensions
+ */
+
+namespace HarperJones\Wordpress\Theme\Feature;
+
+
+class HidePostTypeFeature implements FeatureInterface
+{
+    protected $postTypesToHide;
+
+    public function register($options = [])
+    {
+        $this->postTypesToHide = (array)$options;
+
+        add_action('template_redirect', [$this, 'filterPostTypes']);
+    }
+
+    public function filterPostTypes()
+    {
+        if ( in_array(get_query_var('post_type'),$this->postTypesToHide) ) {
+            wp_redirect( apply_filters('hj/hideposttype/redirect', home_url()), 302);
+            exit();
+        }
+    }
+}
