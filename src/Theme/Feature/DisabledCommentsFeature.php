01a1ae365439cb8e91f9322258342395cfab4438
Peter Eussen <peter.eussen@harperjones.nl>
2016-02-12 12:18:50 +0100

    Add exceptions and also by default disable for CPT

--- src/Theme/Feature/DisabledCommentsFeature.php
+++ src/Theme/Feature/DisabledCommentsFeature.php
@@ -20,16 +20,23 @@ namespace HarperJones\Wordpress\Theme\Feature;
  */
 class DisabledCommentsFeature implements FeatureInterface
 {
+    protected $exceptions = [];
+
     public function register($options = [])
     {
+        $this->exceptions = (!is_bool($options) ? (array)$options : []);
+
         add_action('init',[$this,'removeCommentSupport'],99);
     }
 
     public function removeCommentSupport()
     {
         $pt = get_post_types(['_builtin' => true],'names');
+        $cpt= get_post_types('','names');
 
-        foreach( $pt as $postType ) {
+        $allpt = array_diff(array_merge($pt,$cpt),$this->exceptions);
+
+        foreach( $allpt as $postType ) {
             remove_post_type_support($postType,'comments');
         }
     }
