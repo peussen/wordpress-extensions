c9ccbc93dcb12833b6e64c433c42e91cd4380afd
Peter Eussen <peter.eussen@harperjones.nl>
2016-02-25 17:09:44 +0100

    Code cleanup

--- src/Setup.php
+++ src/Setup.php
@@ -240,83 +240,8 @@ class Setup
 	static public function globalizeStaticMethods($class)
 	{
 		Globalizer::globalizeStaticMethods($class);
-
-//		try {
-//			$reflection = new \ReflectionClass($class);
-//			$methods    = $reflection->getMethods(\ReflectionMethod::IS_STATIC);
-//			$mapping    = array();
-//
-//			foreach( $methods as $method ) {
-//				if ( $method->isPublic()) {
-//					$docblock = $method->getDocComment();
-//
-//					if ( preg_match('/\*\s+@globalize\s*([a-zA-Z0-9_]+)?/ms',$docblock,$matches)) {
-//
-//						if ( isset($matches[1])) {
-//							$global_function = $matches[1];
-//						} else {
-//							$global_function = strtolower(str_replace('\\','_',$method->class) . $method->name);
-//						}
-//						self::createAliasFunction($method->class . '::' . $method->name,$global_function);
-//						$mapping[$global_function] = $method->class . '::' . $method->name;
-//					}
-//				}
-//			}
-//			return $mapping;
-//		} catch (\Exception $e) {
-//			return false;
-//		}
 	}
 
-	/**
-	 * Aliases a method to a global function
-	 *
-	 * @param string $methodName
-	 * @param string $aliasName
-	 *
-	 * @return bool
-	 */
-	static function createAliasFunction($methodName, $aliasName)
-	{
-		if( function_exists($aliasName) )
-			return false;
-
-		$rf         = new \ReflectionMethod($methodName);
-		$fproto     = $aliasName.'(';
-		$fcall      = $methodName.'(';
-		$need_comma = false;
-
-		foreach($rf->getParameters() as $param)
-		{
-			if($need_comma)
-			{
-				$fproto .= ',';
-				$fcall .= ',';
-			}
-
-			$fproto .= '$'.$param->getName();
-			$fcall .= '$'.$param->getName();
-
-			if($param->isOptional() && $param->isDefaultValueAvailable())
-			{
-				$val = $param->getDefaultValue();
-				if(is_string($val)) {
-					$val = "'" . addslashes($val) . "'";
-				} else if ( is_bool($val)) {
-					$val = $val ? 'true' : 'false';
-				}
-				$fproto .= ' = '.$val;
-			}
-			$need_comma = true;
-		}
-		$fproto .= ')';
-		$fcall  .= ')';
-
-		$f = "function $fproto {return ".$fcall.';}';
-
-		eval($f);
-		return true;
-	}
 
 	static public function dashedToClass($featureorfilter)
 	{
