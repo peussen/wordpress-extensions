a1068d3b1e3095b7fefff2fd338a9ad0eb23b136
Peter Eussen <peter.eussen@harperjones.nl>
2016-04-15 08:59:08 +0200

    Add bugherd toolbar to a project (requires an apikey)

---
+++ src/Theme/Feature/BugherdFeature.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * @author: petereussen
+ * @package: wordpress-extensions
+ */
+
+namespace HarperJones\Wordpress\Theme\Feature;
+
+
+class BugherdFeature implements FeatureInterface
+{
+  public function register($options = [])
+  {
+    if ( isset($options['key'])) {
+      $apiKey = $options['key'];
+    } else if ( isset($options[0])) {
+      $apiKey = $options[0];
+    } else if ( empty($options)) {
+      $apiKey = $options;
+    }
+
+    add_action('wp_head',function() use ($apiKey) {
+      ?>
+      <script type='text/javascript'>
+        (function (d, t) {
+          var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+          bh.type = 'text/javascript';
+          bh.src = 'https://www.bugherd.com/sidebarv2.js?apikey=<?php echo addslashes($apiKey); ?>';
+          s.parentNode.insertBefore(bh, s);
+        })(document, 'script');
+      </script>
+      <?php
+    });
+  }
+
+
+}
